fd39dc3b0852b76acd9509a8a6612ce0e65e0831
Leonard <leo@leonard-lemke.com>
2024-12-12 21:32:52 +0100

    fix bottom sheet + color handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ allprojects {
 
 val groupId = "io.github.lemkinator"
 val artifact = "common-utils"
-val versionName = "0.1.9"
+val versionName = "0.2.2"
 
 subprojects {
     afterEvaluate {
--- lib/src/main/java/de/lemke/commonutils/widget/InfoBottomSheet.kt
+++ lib/src/main/java/de/lemke/commonutils/widget/InfoBottomSheet.kt
@@ -9,40 +9,38 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.StringRes
-import androidx.fragment.app.FragmentActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import de.lemke.commonutils.databinding.WidgetInfoBottomsheetBinding
 import kotlin.apply
 
-class InfoBottomSheet(
-    private val activity: FragmentActivity,
-    private val title: String = "",
-    private val message: String = "",
-    private val onDismissListener: DialogInterface.OnDismissListener? = null,
-    private val textGravity: Int = Gravity.CENTER
-) : BottomSheetDialogFragment() {
+class InfoBottomSheet: BottomSheetDialogFragment() {
     private lateinit var binding: WidgetInfoBottomsheetBinding
 
-    constructor(
-        activity: FragmentActivity,
-        @StringRes title: Int,
-        @StringRes message: Int,
-        onDismissListener: DialogInterface.OnDismissListener? = null,
-        textGravity: Int = Gravity.CENTER
-    ) : this(
-        activity = activity,
-        title = activity.getString(title),
-        message = activity.getString(message),
-        onDismissListener = onDismissListener,
-        textGravity = textGravity
-    )
+    companion object {
+        fun newInstance(
+            title: String,
+            message: String,
+            textGravity: Int = Gravity.CENTER
+        ): InfoBottomSheet {
+            return InfoBottomSheet().apply{
+                arguments = Bundle().apply {
+                    putString(KEY_TITLE, title)
+                    putString(KEY_MESSAGE, message)
+                    putInt(KEY_TEXT_GRAVITY, textGravity)
+                }
+            }
+        }
+
+        const val KEY_TITLE = "key_title"
+        const val KEY_MESSAGE = "key_message"
+        const val KEY_TEXT_GRAVITY = "key_text_gravity"
+    }
 
     override fun onDismiss(dialog: DialogInterface) {
         super.onDismiss(dialog)
-        onDismissListener?.onDismiss(dialog)
+        //onDismissListener?.onDismiss(dialog)
     }
 
     override fun onCreateView(
@@ -60,13 +58,10 @@ class InfoBottomSheet(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.widgetInfoTitle.text = title
+        val textGravity = arguments?.getInt(KEY_TEXT_GRAVITY) ?: Gravity.CENTER
+        binding.widgetInfoTitle.text = arguments?.getString(KEY_TITLE) ?: ""
         binding.widgetInfoTitle.gravity = textGravity
-        binding.widgetInfoMessage.text = message
+        binding.widgetInfoMessage.text = arguments?.getString(KEY_MESSAGE) ?: ""
         binding.widgetInfoMessage.gravity = textGravity
-    }
-
-    fun show() {
-        show(activity.supportFragmentManager, InfoBottomSheet::class.java.simpleName)
     }
 }
