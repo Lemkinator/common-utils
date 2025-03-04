68c4a412e7e0e3d1878ef117653a8402ce5521b5
Leonard <leo@leonard-lemke.com>
2025-03-04 18:22:47 +0100

    add reset string

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-        classpath("com.android.tools.build:gradle:8.8.1")
+        classpath("com.android.tools.build:gradle:8.8.2")
     }
 }
 
@@ -68,7 +68,7 @@ allprojects {
 
 val groupId = "io.github.lemkinator"
 val artifact = "common-utils"
-val versionName = "0.7.1"
+val versionName = "0.7.2"
 
 subprojects {
     afterEvaluate {
