0282faec873c1d8f7dcd7d7f23d95b28d36413e2
Leonard <leo@leonard-lemke.com>
2025-05-08 22:17:44 +0200

    Update oneui-design dependency to version 0.5.13

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("sesl.androidx.appcompat:appcompat:1.7.0+1.0.34-sesl6+rev8")
     //SESL6(OneUI 6) Material Components + Design Lib + Icons
     implementation("sesl.com.google.android.material:material:1.12.0+1.0.23-sesl6+rev3")
-    implementation("io.github.tribalfs:oneui-design:0.5.12+oneui6")
+    implementation("io.github.tribalfs:oneui-design:0.5.13+oneui6")
     implementation("io.github.oneuiproject:icons:1.1.0")
 
     implementation("com.airbnb.android:lottie:6.6.6")
