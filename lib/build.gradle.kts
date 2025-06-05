87fe3fb1218179aa8f5881870c0834c93e5f6e82
Leonard <leo@leonard-lemke.com>
2025-06-05 20:03:05 +0200

    Update lifecycle-runtime-ktx dependency and improve GitHub credentials handling

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("io.github.oneuiproject:icons:1.1.0")
 
     implementation("com.airbnb.android:lottie:6.6.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("com.google.android.gms:play-services-oss-licenses:17.1.0")
     implementation("com.google.android.play:app-update-ktx:2.1.0")
     implementation("com.google.android.play:review-ktx:2.0.2")
