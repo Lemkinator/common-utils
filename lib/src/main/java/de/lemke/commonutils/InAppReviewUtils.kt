23c5dbc18d33882f70667769381639d49ef5a27b
Leonard <leo@leonard-lemke.com>
2025-06-16 21:49:27 +0200

    Fix Duplicate Code, resolves #9

--- lib/src/main/java/de/lemke/commonutils/InAppReviewUtils.kt
+++ lib/src/main/java/de/lemke/commonutils/InAppReviewUtils.kt
@@ -6,6 +6,8 @@ import android.content.Context.MODE_PRIVATE
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import com.google.android.gms.tasks.Task
+import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManagerFactory
 import java.lang.System.currentTimeMillis
 import java.util.concurrent.TimeUnit.MILLISECONDS
@@ -13,7 +15,9 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 private const val tag = "InAppReviewUtils"
 
 fun AppCompatActivity.getLastInAppReview() = getSharedPreferences(tag, MODE_PRIVATE).getLong("lastInAppReview", currentTimeMillis())
+
 fun AppCompatActivity.setInAppReview() = getSharedPreferences(tag, MODE_PRIVATE).edit { putLong("lastInAppReview", currentTimeMillis()) }
+
 fun AppCompatActivity.canShowInAppReview() = try {
     val daysSinceLastReview = MILLISECONDS.toDays(currentTimeMillis() - getLastInAppReview())
     Log.d(tag, "Days since last review: $daysSinceLastReview")
@@ -23,41 +27,29 @@ fun AppCompatActivity.canShowInAppReview() = try {
     false
 }
 
-fun AppCompatActivity.showInAppReviewOrFinish() {
-    try {
-        if (canShowInAppReview()) {
-            Log.d(tag, "In app review requested less than 14 days ago, skipping")
-            finishAfterTransition()
-            return
-        }
-        Log.d(tag, "trying to show in app review")
-        setInAppReview()
-        val manager = ReviewManagerFactory.create(this)
-        val request = manager.requestReviewFlow()
-        request.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Log.d(tag, "Review task successful")
-                val reviewInfo = task.result
-                val flow = manager.launchReviewFlow(this, reviewInfo)
-                flow.addOnCompleteListener {
-                    Log.d(tag, "Review flow complete")
-                    finishAfterTransition()
-                }
-            } else {
-                Log.e(tag, "Review task failed: ${task.exception?.message}")
-                finishAfterTransition()
-            }
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-        finishAfterTransition()
-    }
+fun AppCompatActivity.showInAppReviewOrFinish() = showInAppReviewAnd { finishAfterTransition() }
+
+fun AppCompatActivity.showInAppReviewIfPossible() = showInAppReview()
+
+private fun AppCompatActivity.showInAppReviewAnd(action: () -> Unit) {
+    showInAppReview(
+        onNotAllowed = { action() },
+        onCompleted = { action() },
+        onReviewError = { task -> action() },
+        onError = { e -> action() }
+    )
 }
 
-fun AppCompatActivity.showInAppReviewIfPossible() {
+private fun AppCompatActivity.showInAppReview(
+    onNotAllowed: () -> Unit = {},
+    onCompleted: () -> Unit = {},
+    onReviewError: (Task<ReviewInfo>) -> Unit = {},
+    onError: (Exception) -> Unit = {},
+) {
     try {
         if (canShowInAppReview()) {
             Log.d(tag, "In app review requested less than 14 days ago, skipping")
+            onNotAllowed()
             return
         }
         Log.d(tag, "trying to show in app review")
@@ -71,12 +63,15 @@ fun AppCompatActivity.showInAppReviewIfPossible() {
                 val flow = manager.launchReviewFlow(this, reviewInfo)
                 flow.addOnCompleteListener {
                     Log.d(tag, "Review flow complete")
+                    onCompleted()
                 }
             } else {
                 Log.e(tag, "Review task failed: ${task.exception?.message}")
+                onReviewError(task)
             }
         }
     } catch (e: Exception) {
         e.printStackTrace()
+        onError(e)
     }
 }
