fb638004170590b877e08fa9a90dc8e3aeb9e0de
Leonard <leo@leonard-lemke.com>
2025-06-23 00:20:54 +0200

    Use `withStyledAttributes` to simplify attribute handling
    This change replaces `context.obtainStyledAttributes` and `recycle()` with the more concise `context.withStyledAttributes` in the `DimmingView` constructor. This Kotlin extension function automatically handles the recycling of the `TypedArray`.

--- lib/src/main/java/de/lemke/commonutils/ui/widget/DimmingView.kt
+++ lib/src/main/java/de/lemke/commonutils/ui/widget/DimmingView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import androidx.core.content.withStyledAttributes
 import de.lemke.commonutils.R
 
 open class DimmingView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
@@ -17,9 +18,8 @@ open class DimmingView @JvmOverloads constructor(context: Context, attrs: Attrib
             layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
             setBackgroundColor((DEFAULT_ALPHA * 255.0f + 0.5f).toInt() shl 24)
         } else {
-            context.obtainStyledAttributes(attrs, R.styleable.DimmingView).apply {
+            context.withStyledAttributes(attrs, R.styleable.DimmingView) {
                 setBackgroundColor((getFloat(R.styleable.DimmingView_alpha, DEFAULT_ALPHA) * 255.0f + 0.5f).toInt() shl 24)
-                recycle()
             }
         }
     }
