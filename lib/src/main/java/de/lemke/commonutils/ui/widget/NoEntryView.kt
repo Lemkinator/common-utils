c8a8cbacaa695c83d14241723292e07db02369fe
Leonard <leo@leonard-lemke.com>
2025-06-23 00:22:33 +0200

    Add NoEntryView custom widget
    This widget displays a Lottie animation and text to indicate that there are no entries. It can be used to show a placeholder when a list or view is empty.
    
    Key features:
    - Customizable text via XML attribute or programmatically.
    - Lottie animation with configurable color filter.
    - Helper methods to toggle visibility based on list emptiness or explicit boolean.

---
+++ lib/src/main/java/de/lemke/commonutils/ui/widget/NoEntryView.kt
@@ -0,0 +1,63 @@
+@file:Suppress("unused")
+
+package de.lemke.commonutils.ui.widget
+
+import android.content.Context
+import android.graphics.ColorFilter
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.withStyledAttributes
+import androidx.core.view.isVisible
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieProperty.COLOR_FILTER
+import com.airbnb.lottie.SimpleColorFilter
+import com.airbnb.lottie.model.KeyPath
+import com.airbnb.lottie.value.LottieValueCallback
+import de.lemke.commonutils.R
+import dev.oneuiproject.oneui.widget.RoundedLinearLayout
+
+
+class NoEntryView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : RoundedLinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+    private val noEntryLottie by lazy { findViewById<LottieAnimationView>(R.id.noEntryLottie) }
+
+    init {
+        inflate(context, R.layout.widget_no_entry_view, this)
+        if (attrs != null) {
+            context.withStyledAttributes(attrs, R.styleable.NoEntryView) {
+                getText(R.styleable.NoEntryView_noEntryText)?.let {
+                    findViewById<TextView>(R.id.noEntryText).text = it
+                }
+            }
+        }
+    }
+
+    fun toggle(otherView: View? = null) = updateVisibility(!isVisible, otherView)
+    fun updateVisibilityWith(list: List<*>, otherView: View? = null) = updateVisibility(list.isEmpty(), otherView)
+    fun updateVisibility(visible: Boolean, otherView: View? = null) = if (visible) {
+        otherView?.isVisible = false
+        show()
+    } else {
+        hide()
+        otherView?.isVisible = true
+    }
+
+    fun show() {
+        noEntryLottie.cancelAnimation()
+        noEntryLottie.progress = 0f
+        isVisible = true
+        val callback = LottieValueCallback<ColorFilter>(SimpleColorFilter(context.getColor(R.color.primary_color_themed)))
+        noEntryLottie.addValueCallback(KeyPath("**"), COLOR_FILTER, callback)
+        noEntryLottie.postDelayed({ noEntryLottie.playAnimation() }, 400)
+    }
+
+    fun hide() {
+        isVisible = false
+    }
+
+}
