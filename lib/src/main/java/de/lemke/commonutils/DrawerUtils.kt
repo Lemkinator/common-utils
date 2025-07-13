377c9c8a5df3cd4d3806fc5f502b84a1c2ea7600
Leonard <leo@leonard-lemke.com>
2025-07-13 23:05:47 +0200

    Add dot badges to drawer and nav rail when update is available

--- lib/src/main/java/de/lemke/commonutils/DrawerUtils.kt
+++ lib/src/main/java/de/lemke/commonutils/DrawerUtils.kt
@@ -6,6 +6,7 @@ import android.widget.ImageButton
 import androidx.appcompat.content.res.AppCompatResources
 import com.google.android.material.navigation.NavigationView
 import de.lemke.commonutils.ui.activity.CommonUtilsAboutActivity
+import dev.oneuiproject.oneui.layout.Badge
 import dev.oneuiproject.oneui.layout.NavDrawerLayout
 import dev.oneuiproject.oneui.navigation.widget.DrawerNavigationView
 import dev.oneuiproject.oneui.R as iconsR
@@ -21,6 +22,7 @@ fun NavDrawerLayout.setupHeaderAndNavRail(aboutApp: String) {
     )
     setNavRailContentMinSideMargin(14)
     closeNavRailOnBack = true
+    context.onAppUpdateAvailable { setButtonBadges(Badge.DOT, Badge.DOT) }
 }
 
 private var lastClick = 0L
